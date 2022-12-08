0ec48477a84872e2116aefbdc1f24019db798b94
nunixnunix04 <nunixnunix@gmail.com>
2022-12-08 00:43:19 -0500

    finished a demo of how local PGS accuracy can be visualized

--- sandbox/generate_test_data.R
+++ sandbox/generate_test_data.R
@@ -16,24 +16,38 @@ for (j in 1:L) {
 }
 
 # generates fake output data based on PC data
-PC_coeffs <- rnorm(L)
+set.seed(2016)
+#PC_coeffs <- rnorm(L) * 1/(1:32)
+full_data$origin_dist <- as.numeric(NA)
 full_data$PGS <- as.numeric(NA)
 full_data$phenotype <- rnorm(N)
-PC_error_prop <- 0.5
-absolute_error_prop <- 1
-for (i in 1:nrow(data)) {
-  pos <- data[i,2:(1+L)] %>% unlist() %>% unname()
-  PC_error <- PC_coeffs %*% pos
+PC_error_prop <- 1
+absolute_error_prop <- 0.5
+for (i in 1:nrow(full_data)) {
+  pos <- full_data[i,2:(1+L)] %>% unlist() %>% unname()
+  origin_dist <- sum(full_data[i,2:(1+L)]**2)
+  full_data$origin_dist[i] <- origin_dist
+  
+  #PC_error <- PC_coeffs %*% pos
   #origin_dist <- dist(rbind(data[i,2:(1+L)], rep(0,L)))[[1]]
-  PGS <- full_data$phenotype[i] + PC_error_prop*(PC_error) + absolute_error_prop*rnorm(1)
+  PGS <- full_data$phenotype[i] + PC_error_prop*(origin_dist)*rnorm(1) + absolute_error_prop*rnorm(1)
   full_data$PGS[i] <- PGS
   if (i %% 100 == 0) {print(i)}
+  #if (is.na(PGS)) {print(i)}
 }
+
+# writes out data
+write.table(full_data, "full_test_data.txt", sep="\t",row.names=FALSE, quote=FALSE)
+
 # plots (PGS-phenotype) by PC1
 ggplot(full_data, aes(x=PC3, y=(PGS-phenotype))) +
   geom_point(alpha=0.03) +
   geom_smooth(method="lm", color="blue") +
   geom_abline(slope=PC_coeffs[3], intercept = 0)
+
+ggplot(full_data, aes(x=origin_dist, y=abs((PGS-phenotype)))) +
+  geom_point(alpha=0.03) +
+  geom_smooth(method="lm", color="blue")
 
 # plots PGS vs phenotype
 ggplot(full_data, aes(x=phenotype, y=PGS)) +
@@ -43,5 +57,3 @@ cor.test(full_data$PGS, full_data$phenotype)
 # plots PC data for first two dimensions
 ggplot(full_data, aes(x=PC1, y=PC2)) +
   geom_point(alpha = 0.1)
-
-write.table(full_data, "full_test_data.txt", sep="\t",row.names=FALSE, quote=FALSE)
