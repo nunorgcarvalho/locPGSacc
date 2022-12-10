dfbf009dc1ee6f28bc64293e7a841d0e278ff0e5
nunixnunix04 <nunixnunix@gmail.com>
2022-12-10 12:36:25 -0500

    implemented data imputation using DINEOF

--- sandbox/exposome_PCA.R
+++ sandbox/exposome_PCA.R
@@ -31,3 +31,29 @@ pheno_matrix2 <- pheno_matrix %>% mutate(PC1 = pca1_tibble$PC1)
 
 lm1 <- lm(PC1 ~ ., data=pheno_matrix2)
 summary(lm1)
+
+####
+# interpolating, subset
+library(sinkr)
+
+pheno_subset <- pheno %>% select(starts_with("f"), -FID) %>%
+  filter(row_number() %in% sample(1:nrow(pheno),40000,replace=FALSE))
+
+dineof1 <- dineof(as.matrix(pheno_subset))
+pheno_subset_imputed <- as_tibble(dineof1$Xa)
+
+pca2 <- prcomp(pheno_subset_imputed)
+
+pca2_tibble <- as_tibble(pca2$x)
+summary(pca2)
+
+# interpolating, full
+pheno_matrix3 <- pheno %>% select(starts_with("f"), -FID)
+dineof2 <- dineof(as.matrix(pheno_matrix3))
+
+pheno_imputed <- as_tibble(dineof2$Xa)
+
+pca3 <- prcomp(pheno_imputed)
+
+pca3_tibble <- as_tibble(pca3$x)
+summary(pca3)
