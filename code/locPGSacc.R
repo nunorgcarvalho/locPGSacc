c623a67a648fa0eab924f3fa2a336ca0a01fc1ff
nunixnunix04 <nunixnunix@gmail.com>
2023-06-20 15:02:19 -0400

    added hybrid mode (fr+k) for computing nearest neighbors

--- code/locPGSacc.R
+++ code/locPGSacc.R
@@ -2,43 +2,57 @@ library(tidyverse)
 library(data.table)
 library(dbscan)
 
-locPGSacc <- function(
+locPGSacc <- function (
     data,
     col_dims,
     col_pheno,
     col_PGS,
     R = -1,
     k = -1,
-    fixed_radius=TRUE
+    mode = "fr" # fr=fixed-radius, k=k closest neighbors, hybrid=highest n_neighbors of fr and k
     ) {
   
   # Checks if data table already contains an output tha
   cols_needed <- c("n_neighbors","locPGSacc")
   if (any(cols_needed %in% colnames(data))) {
     cols_duplicated <- cols_needed[cols_needed %in% colnames(data)]
-    print(cat("ERROR: Data table already contains at least one column used for output purposes:", cols_duplicated))
-    stop()
+    stop(paste0(c("Data table already contains at least one column used for output purposes:", cols_duplicated), collapse=" "))
   }
   
   data_dims <- data %>% select(any_of(col_dims))
   n_samples <- nrow(data_dims)
+  if (k >= n_samples) {warning("k=",k," is bigger than number of samples")}
   
   t1 <- Sys.time()
-  if (fixed_radius) {
-    print(paste0("Computing fixed-radius=",r," nearest neighbors for ",n_samples," samples"))
+  if (mode=="fr") {
+    # Fixed-radius mode
+    print(paste0("Computing fixed-radius=",R," nearest neighbors for ",n_samples," samples"))
     NN <- dbscan::frNN(data_dims, eps=R, sort=FALSE)
     NN_ids <- NN$id
     data$n_neighbors <- sapply(NN$id, length)
-  } else {
+  } else if (mode=="k") {
+    # k-nearest mode
     print(paste0("Computing k=",k," nearest neighbors for ",n_samples," samples"))
     NN <- dbscan::kNN(data_dims, k=k, sort=FALSE)
     NN_ids <- split(NNk$id,seq_len(nrow(NNk$id)))
     data$n_neighbors <- k
+  } else if (mode=="hybrid") {
+    # hybrid fr+k mode
+    print(paste0("Computing hybrid fixed-radius=",R," & k=",k," nearest neighbors for ",n_samples," samples"))
+    # computes NN with fixed-radius
+    NNfr <- dbscan::frNN(data_dims, eps=R, sort=FALSE)
+    NN_ids <- NNfr$id
+    # computes NN with k-nearest for points with <k fixed-radius neighbors
+    small_neighborhoods <- (1:nrow(data))[sapply(NNfr$id, length) < k]
+    NNk <- dbscan::kNN(data_dims, k=k, sort=FALSE, query=data_dims[small_neighborhoods,])
+    NN_ids[small_neighborhoods] <- split(NNk$id,seq_len(nrow(NNk$id)))
+    data$n_neighbors <- sapply(NN_ids, length)
   }
   Sys.time() - t1
   
   print("Computing correlation within each neighborhood")
   r_values <- c()
+  # computes correlation between phenotype and PGS for phenotype
   for (i in 1:nrow(data)) {
     r <- cor(data[ NN_ids[[i]], ][[ col_pheno ]],
              data[ NN_ids[[i]], ][[ col_PGS ]])
--- sandbox/function_testing.R
+++ sandbox/function_testing.R
@@ -1,7 +1,7 @@
 setwd("~/group_nuno/locPGSacc/code/")
 source("../code/locPGSacc.R")
 
-if (!exists("data")) {
+if (!exists("data_raw")) {
   data_raw <- as_tibble(fread("~/group_nuno/locPGSacc/scratch/pheno.txt"))
   N <- 10000
   i_keep <- sample(1:nrow(data_raw),N, replace = FALSE)
@@ -12,26 +12,25 @@ col_dims <- paste0("pc",1:40)
 col_pheno <- "BMI"
 col_PGS <- "BMI_PGS"
 R <- 25
-k <- 100
-
+k <- 500
 
 data_output <- locPGSacc(data,
                          col_dims = col_dims,
                          col_pheno = col_pheno,
                          col_PGS = col_PGS,
-                         #R = R,
+                         R = R,
                          k = k,
-                         fixed_radius=FALSE)
+                         mode="hybrid"
+                         )
 
 colnames(data_output)
 hist(log2(data_output$n_neighbors))
 ggplot(data_output, aes(x=pc1, y=locPGSacc)) +
   geom_point(alpha=0.1) + geom_smooth(method='lm')
-# ggplot(data_output, aes(x=n_neighbors, y=locPGSacc)) +
-#   geom_point(alpha=0.1) + geom_smooth(method='lm') +xlim(200,2000)
 
 dim_means <- colMeans(data_output %>% select(all_of(col_dims)))
 data_output <- data_output %>%
   mutate(PC_dist = sqrt(rowSums((select(., all_of(col_dims)) - dim_means)^2)))
-ggplot(data_output[data_output$PC_dist<50,], aes(x=PC_dist, y=locPGSacc)) +
-  geom_point(alpha=0.02) + geom_smooth(method='lm')
+ggplot(data_output, #[data_output$PC_dist<50,],
+       aes(x=PC_dist, y=locPGSacc)) +
+  geom_point(alpha=1, aes(color=log2(n_neighbors))) + geom_smooth(method='lm')
